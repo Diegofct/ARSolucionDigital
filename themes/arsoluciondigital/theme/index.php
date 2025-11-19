ac6abb5a3cecfabe99b45bfbf8e01688527a9dd7
Diegofct <96diegofct@gmail.com>
2025-11-19 12:29:13 -0500

    The main section (hero-content) is developed, the header-content is adjusted, and the style is changed to have a background with a glow effect and also adjusted to adapt to all types of screens.

--- themes/arsoluciondigital/theme/index.php
+++ themes/arsoluciondigital/theme/index.php
@@ -16,7 +16,35 @@ get_header();
 ?>
 
 	<main id="main" class="site-main">
+		<div class="container mx-auto container-padding py-12">
 
+			<?php
+			if ( have_posts() ) :
+
+				// Load posts loop.
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content/content', get_post_type() );
+				endwhile;
+
+				// Previous/next page navigation.
+				the_posts_pagination(
+					array(
+						'mid_size'  => 2,
+						'prev_text' => __( 'Anterior', 'arsoluciondigital' ),
+						'next_text' => __( 'Siguiente', 'arsoluciondigital' ),
+					)
+				);
+
+			else :
+
+				// If no content, include the "No posts found" template.
+				get_template_part( 'template-parts/content/content', 'none' );
+
+			endif;
+			?>
+
+		</div>
 	</main>
 
 <?php
